a37d86a229533a0d7736a19c0159055b54b6bc4c
wheetcracker <57098043+wheetcracker@users.noreply.github.com>
2022-03-15 16:49:16 -0500

    Migration to cum bis addon

--- cum_main_functions.lua
+++ cum_main_functions.lua
@@ -1,5 +1,5 @@
-function search_table(itemname)
-    for index, value in next, Donkey_loot_table do
+function cum_search_table(itemname)
+    for index, value in next, Loading_bis_table do
         if value["loot_id"] == itemname then
             return value["prio"]
         end
